f9a1bb65445fce751530c442e0e2d1f5076e0831
Anahit Yeghyan <anahit.yeghyann@gmail.com>
2026-03-14 12:34:16 +0400

    Add django-smart-selects, update admin and gitignore

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
+from .models import (
+    Service, ServiceBranch, ServiceAction,
+    Telecom, TelecomBranch, TelecomAction,
+    Government, GovernmentBranch, GovernmentAction,
+    Bank, BankBranch, BankAction
+)
 
-# Register your models here.
+# Register all models
+admin.site.register(Service)
+admin.site.register(ServiceBranch)
+admin.site.register(ServiceAction)
+
+admin.site.register(Telecom)
+admin.site.register(TelecomBranch)
+admin.site.register(TelecomAction)
+
+admin.site.register(Government)
+admin.site.register(GovernmentBranch)
+admin.site.register(GovernmentAction)
+
+admin.site.register(Bank)
+admin.site.register(BankBranch)
+admin.site.register(BankAction)
